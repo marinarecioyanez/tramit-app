25737d78241918442c8d3a6b4791c5316d2bb023
marinarecioyanez <marinarecioyanez@gmail.com>
2026-05-18 19:54:22 +0200

    Import CalendariFiscalClient in admin-client.tsx

--- components/features/admin-client.tsx
+++ components/features/admin-client.tsx
@@ -14,6 +14,7 @@ import {
   RefreshCw, Filter
 } from 'lucide-react'
 import { SettingsClient } from './settings-client'
+import { CalendariFiscalClient } from './calendari-fiscal-client'
 
 interface ProfileData {
   id: string; full_name: string; email: string; role: string
